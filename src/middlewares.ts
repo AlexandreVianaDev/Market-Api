acf43c1270458e36ec531ec2c326f137f177f143
AlexandreVianaDev <alexandreviana94@hotmail.com>
2023-03-29 20:42:24 -0300

    feat: middleware for check names

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,3 +1,46 @@
-//Verificação de nome existente
+import { NextFunction, Request, Response } from "express";
+import { market } from "./database";
+import { IProduct } from "./interfaces";
 
-//Verificação se o id buscado existe
+export const verifyIfNameExistsPost = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Response | void => {
+  const newProducts = req.body;
+  console.log(req.body);
+
+  const productFind: IProduct[] = newProducts.filter((product: IProduct) =>
+    market.some((prod) => prod.name == product.name)
+  );
+
+  if (productFind.length > 0) {
+    return res.status(409).json({
+      error: "Product already registered",
+    });
+  }
+
+  return next();
+
+  //   res.locals.product = productFind
+};
+
+export const verifyIfNameExistsPatch = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Response | void => {
+  const newProducts = req.body;
+  console.log(req.body);
+
+  const productFind = market.some((prod) => prod.name == newProducts.name);
+
+  if (productFind) {
+    return res.status(404).json({
+      error: "Product not found",
+    });
+  }
+
+  //   res.locals.product = productFind
+  return next();
+};
